862e990b1941c3a9e65a284e8b775e6bffba92d8
Samuel Igwesi <95619877+SamuelIgwesi@users.noreply.github.com>
2022-11-08 13:18:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,72 @@
 const express = require("express");
+coconst express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const mailChimp = require("@mailchimp/mailchimp_marketing");
+const dotenv = require("dotenv");
 
 const app = express();
+require("dotenv").config()
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html")
 });
 
 mailChimp.setConfig({
- apiKey: "b6eb0353f539f96283aad098cac53ba6-us14",
- server: "us14"
+  apiKey: process.env.apiKey,
+  server: process.env.server
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
 
-  var data={
-    members:[{
+  var data = {
+    members: [{
       email_address: email,
       status: "subscribed",
-      merge_fields:{
+      merge_fields: {
         FNAME: firstName,
         LNAME: lastName
       }
     }]
   }
 
-  const jsonData= JSON.stringify(data);
-const url="https://us14.api.mailchimp.com/3.0/lists/62260d3a19";
-const options={
-  method:"POST",
-  auth:"Prime Developer:b6eb0353f539f96283aad098cac53ba6-us14"
-}
-
-// On success send users to success, otherwise on failure template
-const request=https.request(url,options,function(response){
-  if(response.statusCode===200){
-    res.sendFile(__dirname+"/success.html");
-  }else{
-    res.sendFile(__dirname+"/failure.html");
+  const jsonData = JSON.stringify(data);
+  const url = process.env.mongoUrl;
+  const options = {
+    method: "POST",
+    auth: process.env.mongoAuth
   }
-  response.on("data",function(data){
-    console.log(JSON.parse(data));
+
+  // On success send users to success, otherwise on failure template
+  const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
   });
-});
   request.write(jsonData);
   request.end();
 });
 
 // Failure route
-app.post("/failure",function(req,res){
-   res.redirect("/");
+app.post("/failure", function(req, res) {
+  res.redirect("/");
 })
 
-app.listen(process.env.PORT||3000, function () {
- console.log("Server is running at port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running at port 3000");
 });
